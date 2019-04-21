0a7867080987ce75fd345ede8a741d5e0cca3958
Matthew Day <daystar.1998@yahoo.com>
2019-04-20 21:07:16 -0500

    Implement drawLine method

--- Rubik's Race/src/engine/graphics/DisplayPanel.java
+++ Rubik's Race/src/engine/graphics/DisplayPanel.java
@@ -37,7 +37,7 @@ public class DisplayPanel extends JPanel implements IDisplay {
 
 		image = new BufferedImage(width, height,
 				BufferedImage.TYPE_INT_RGB);
-		pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+		pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
 	}
 
 	@Override
@@ -49,12 +49,66 @@ public class DisplayPanel extends JPanel implements IDisplay {
 	@Override
 	public void drawPixel(Vector2f position, Color color) {
 
-		pixels[(int)position.y * image.getWidth() + (int)position.x] = color.getRGB();
+		pixels[(int) position.y * image.getWidth() + (int) position.x] = color.getRGB();
 	}
 
 	@Override
 	public void drawLine(Vector2f start, Vector2f end, Color color) {
 
+		/*
+			Author: Dana Steil
+			Source: AddLine function in https://www.harding.edu/dsteil/345/assignments/picture.cpp
+
+			Modified by Matthew Day to convert it to Java, use Vector2f, and use the drawPixel method
+	 	*/
+
+		Vector2f temp = new Vector2f();
+
+		//y = mx + b
+		//y - y0 = m( x - x0 )
+		//m=(y-y0)/(x-x0)
+		//x = (y-b)/m;
+		if (start.getX() != end.getX()) {
+
+			double m = (start.getY() - end.getY()) / (start.getX() - end.getX());
+			double b = start.getY() - (m * start.getX());
+
+			if ((int) Math.abs(start.getY() - end.getY()) < (int) Math.abs(start.getX() - end.getX())) {
+
+				int x1 = ((int) start.getX() < (int) end.getX()) ? (int) start.getX() : (int) end.getX();
+				int x2 = ((int) start.getX() < (int) end.getX()) ? (int) end.getX() : (int) start.getX();
+
+				//change in x is greater than change in y
+				for (temp.x = x1; temp.x <= x2; temp.x++) {
+
+					temp.y = (int) Math.floor((m * temp.x) + b);
+					drawPixel(temp, color);
+				}
+			} else {
+
+				//change in y is greater than change in x
+				int y1 = ((int) start.getY() < (int) end.getY()) ? (int) start.getY() : (int) end.getY();
+				int y2 = ((int) start.getY() < (int) end.getY()) ? (int) end.getY() : (int) start.getY();
+
+				//change in x is greater than change in y
+				for (temp.y = y1; temp.y <= y2; temp.y++) {
+
+					temp.x = (int) Math.floor((temp.y - b) / m);
+					drawPixel(temp, color);
+				}
+			}
+		} else {
+
+			int y1 = ((int) start.getY() < (int) end.getY()) ? (int) start.getY() : (int) end.getY();
+			int y2 = ((int) start.getY() < (int) end.getY()) ? (int) end.getY() : (int) start.getY();
+
+			temp.x = start.getX();
+
+			for (temp.y = y1; temp.y <= y2; temp.y++) {
+
+				drawPixel(temp, color);
+			}
+		}
 	}
 
 	@Override
